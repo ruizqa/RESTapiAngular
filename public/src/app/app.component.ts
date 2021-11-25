6c2ba09071b9099221eba63868ec3fc52e347918
ruizqa <89569188+ruizqa@users.noreply.github.com>
2021-11-25 16:57:40 -0600

    displaying on html

--- public/src/app/app.component.ts
+++ public/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component,OnInit } from '@angular/core';
 import { HttpService } from './http.service';
 
 @Component({
@@ -6,7 +6,31 @@ import { HttpService } from './http.service';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'public';
+  tasks = [];
+  task:any;
   constructor(private _httpService: HttpService){}
+
+  ngOnInit(){
+    this.getTasks();
+    this.getTask(4);
+  }
+
+  getTasks(){
+    let tempObservable = this._httpService.getTasks();
+    tempObservable.subscribe((data:any) => {
+      this.tasks = data;
+      console.log('All tasks:',this.tasks)
+    });
+  }
+
+  getTask(id:number){
+    let tempObservable =  this._httpService.getTask(id);
+    tempObservable.subscribe((data:any) => {
+      this.task = data;
+      console.log(`Got your task ${id}`, this.task)});
+    
+  }
+
 }
--- public/src/app/http.service.ts
+++ public/src/app/http.service.ts
@@ -8,22 +8,22 @@ export class HttpService {
 
   constructor(private _http: HttpClient) { 
 
-    this.getTasks();
-    this.getTask(4);
   }
 
   getTasks(){
     // our http response is an Observable, store it in a variable
     let tempObservable = this._http.get('http://localhost:8800/tasks');
+    return tempObservable;
     // subscribe to the Observable and provide the code we would like to do with our data from the response
-    tempObservable.subscribe(data => console.log("Got our tasks!", data));
+    //tempObservable.subscribe(data => console.log("Got our tasks!", data));
  }
 
  getTask(id:number){
   // our http response is an Observable, store it in a variable
   let tempObservable = this._http.get('http://localhost:8800/tasks/' + String(id));
+  return tempObservable;
   // subscribe to the Observable and provide the code we would like to do with our data from the response
-  tempObservable.subscribe(data => console.log(`Got your task ${id}`, data));
+  //tempObservable.subscribe(data => console.log(`Got your task ${id}`, data));
 }
 
 }
